01cfa73f89a1130abcd6b2628ede1f0041b4bd5d
Komal305 <komalrani2861@gmail.com>
2024-07-24 23:13:21 +0530

    setup
    setting done

---
+++ Home-Loan/src/main/java/home/loan/app/HomeLoanApplication.java
@@ -0,0 +1,13 @@
+package home.loan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLoanApplication.class, args);
+	}
+
+}
